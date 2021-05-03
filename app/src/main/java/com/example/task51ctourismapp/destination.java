c827faa430cfb2b6d7d19b4d86b98993ca4058f2
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-05-03 16:39:43 +1000

    destination class created

---
+++ app/src/main/java/com/example/task51ctourismapp/destination.java
@@ -0,0 +1,37 @@
+package com.example.task51ctourismapp;
+
+public class destination {
+    public String imageRes;
+    public String header;
+    public String desc;
+
+    public destination(String imageRes, String header, String desc){
+        this.imageRes = imageRes;
+        this.header = header;
+        this.desc = desc;
+    }
+
+    public void setImageRes(String imageRes) {
+        this.imageRes = imageRes;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImageRes() {
+        return imageRes;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
