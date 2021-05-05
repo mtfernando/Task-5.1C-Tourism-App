77388a35182d36b4db26e51284ae46bb68c0091d
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-05-05 15:23:17 +1000

    MAJOR - Revert MainFragment to MainActivty
    RecyclerViews and the rest of the launch screen are now displayed on the MainActivity.
    
    Destination details will be displayed in the fragment that takes up the entire screen. This functionality is yet to be implemented.

--- app/src/main/java/com/example/task51ctourismapp/MainFragment.java
+++ app/src/main/java/com/example/task51ctourismapp/MainFragment.java
@@ -16,52 +16,12 @@ import java.util.List;
 
 public class MainFragment extends Fragment {
     View view;
-    RecyclerView verticalRecyclerView, horizontalRecyclerView;
-    VerticalRecyclerViewAdapter verticalRecyclerViewAdapter;
-    HorizontalRecyclerViewAdapter horizontalRecyclerViewAdapter;
-    List<Destination> destinationList = new ArrayList<>();
 
-    String[] destinationNameList = {"Melbourne","Brisbane","Tokyo","London", "Colombo", "New York"};
-
-    String[] destinationDescriptionList = {
-            "Visit Melbourne now!",
-            "Visit Brisbane now!",
-            "Visit Tokyo now!",
-            "Visit London now!",
-            "Visit Colombo now!",
-            "Visit New York now!"
-    };
-
-    String[] imageResStringList = {
-            "melbourne",
-            "brisbane",
-            "tokyo",
-            "london",
-            "colombo",
-            "newyork"
-    };
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view =  inflater.inflate(R.layout.fragment_main, container, false);
-        for(int i=0; i<destinationNameList.length; i++){
-            Destination destination = new Destination(imageResStringList[i], destinationNameList[i], destinationDescriptionList[i]);
-            destinationList.add(destination);
-            System.out.println(destination.getHeader().toString());
-        };
-
-        //Setting up the Horizontal RecyclerView
-        horizontalRecyclerView = view.findViewById(R.id.horizontalRecyclerView);
-        horizontalRecyclerViewAdapter = new HorizontalRecyclerViewAdapter(destinationList, getActivity());
-        horizontalRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
-        horizontalRecyclerView.setAdapter(horizontalRecyclerViewAdapter);
-
-        //setting up the Vertical RecyclerView
-        verticalRecyclerView = view.findViewById(R.id.verticalRecyclerView);
-        verticalRecyclerViewAdapter = new VerticalRecyclerViewAdapter(destinationList,getActivity());
-        verticalRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        verticalRecyclerView.setAdapter(verticalRecyclerViewAdapter);
         return view;
     }
 }
